3b4e459ff095dd54844e2f3fcf37c3002b57dc44
Gabriel Hoy <79767159+GabrielHoy@users.noreply.github.com>
2026-02-05 23:58:08 -0500

    Many many more configs for WindowFocus effect! Revamped filters too.

--- src/Effects/WindowFocus/WindowFocus.ts
+++ src/Effects/WindowFocus/WindowFocus.ts
@@ -10,6 +10,11 @@ import type { EffectConfigResolver } from 'src/EffectLib/Bloom/Configs';
 
 const effectConfig = {
     longTermUnfocusTransitionThreshold: 10000,
+    shortTermUnfocusEffectFilter: 'blur(0.35px) grayscale(25%)',
+    shortTermUnfocusEffectTransitionDuration: 0.618,
+    longTermUnfocusEffectFilter: 'blur(1.5px) grayscale(75%) brightness(95%)',
+    longTermUnfocusEffectTransitionDuration: 1,
+    windowRefocusEffectTransitionDuration: 1,
 };
 
 const vsbloom = window.__VSBLOOM__;
@@ -112,7 +117,7 @@ export async function Start(configResolver: EffectConfigResolver) {
         RemoveFocusEffectsAndGetCurrentFocusClasses();
     });
 
-    //react to our config changing
+    /* Short-Term -> Long-Term Unfocus Transition Threshold Mutator */
     const lngTmUnfcsThrsMutator = await bloom.configs.RegisterEffectConfigMutator({
         pathResolver: configResolver.GetPropertyPath('longTermUnfocusTransitionThreshold'),
         internalValueMutator: (changedValue) => {
@@ -124,6 +129,57 @@ export async function Start(configResolver: EffectConfigResolver) {
         }
     });
     janitor.Add(() => bloom.configs.UnregisterEffectConfigMutator(lngTmUnfcsThrsMutator));
+
+    /* Effect Filter Mutators */
+    const shortTermUnfocusEffectFilterMutator = await bloom.configs.RegisterEffectConfigMutator({
+        pathResolver: configResolver.GetPropertyPath('shortTermUnfocusEffectFilter'),
+        internalValueMutator: (changedValue) => {
+            vsbloom.Log('debug', 'short term unfocus effect filter changed to ' + changedValue);
+            effectConfig.shortTermUnfocusEffectFilter = changedValue as string;
+            document.documentElement.style.setProperty('--vsbloom-window-short-term-unfocus-filter', effectConfig.shortTermUnfocusEffectFilter);
+        }
+    });
+    janitor.Add(() => bloom.configs.UnregisterEffectConfigMutator(shortTermUnfocusEffectFilterMutator));
+    const longTermUnfocusEffectFilterMutator = await bloom.configs.RegisterEffectConfigMutator({
+        pathResolver: configResolver.GetPropertyPath('longTermUnfocusEffectFilter'),
+        internalValueMutator: (changedValue) => {
+            vsbloom.Log('debug', 'long term unfocus effect filter changed to ' + changedValue);
+            effectConfig.longTermUnfocusEffectFilter = changedValue as string;
+            document.documentElement.style.setProperty('--vsbloom-window-long-term-unfocus-filter', effectConfig.longTermUnfocusEffectFilter);
+        }
+    });
+    janitor.Add(() => bloom.configs.UnregisterEffectConfigMutator(longTermUnfocusEffectFilterMutator));
+
+    /* Effect Transition Duration Mutators */
+    const shortTermUnfocusEffectTransitionDurationMutator = await bloom.configs.RegisterEffectConfigMutator({
+        pathResolver: configResolver.GetPropertyPath('shortTermUnfocusEffectTransitionDuration'),
+        internalValueMutator: (changedValue) => {
+            vsbloom.Log('debug', 'short term unfocus effect transition duration changed to ' + changedValue);
+            effectConfig.shortTermUnfocusEffectTransitionDuration = changedValue as number;
+            document.documentElement.style.setProperty('--vsbloom-window-short-term-unfocus-anim-duration', effectConfig.shortTermUnfocusEffectTransitionDuration + 's');
+        }
+    });
+    janitor.Add(() => bloom.configs.UnregisterEffectConfigMutator(shortTermUnfocusEffectTransitionDurationMutator));
+    const longTermUnfocusEffectTransitionDurationMutator = await bloom.configs.RegisterEffectConfigMutator({
+        pathResolver: configResolver.GetPropertyPath('longTermUnfocusEffectTransitionDuration'),
+        internalValueMutator: (changedValue) => {
+            vsbloom.Log('debug', 'long term unfocus effect transition duration changed to ' + changedValue);
+            effectConfig.longTermUnfocusEffectTransitionDuration = changedValue as number;
+            document.documentElement.style.setProperty('--vsbloom-window-long-term-unfocus-anim-duration', effectConfig.longTermUnfocusEffectTransitionDuration + 's');
+        }
+    });
+    janitor.Add(() => bloom.configs.UnregisterEffectConfigMutator(longTermUnfocusEffectTransitionDurationMutator));
+
+    /* Window Refocus Effect Transition Duration Mutator */
+    const windowRefocusEffectTransitionDurationMutator = await bloom.configs.RegisterEffectConfigMutator({
+        pathResolver: configResolver.GetPropertyPath('windowRefocusEffectTransitionDuration'),
+        internalValueMutator: (changedValue) => {
+            vsbloom.Log('debug', 'window refocus effect transition duration changed to ' + changedValue);
+            effectConfig.windowRefocusEffectTransitionDuration = changedValue as number;
+            document.documentElement.style.setProperty('--vsbloom-window-refocus-anim-duration', effectConfig.windowRefocusEffectTransitionDuration + 's');
+        }
+    });
+    janitor.Add(() => bloom.configs.UnregisterEffectConfigMutator(windowRefocusEffectTransitionDurationMutator));
 }
 
 export function Stop() {
