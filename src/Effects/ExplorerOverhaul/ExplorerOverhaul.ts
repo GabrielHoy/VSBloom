9cb07920eaa90c43a31d02b799482d3c78b08bc1
Gabriel Hoy <79767159+GabrielHoy@users.noreply.github.com>
2026-02-14 22:59:02 -0500

    re-add ExplorerOverhaul post-update

---
+++ src/Effects/ExplorerOverhaul/ExplorerOverhaul.ts
@@ -0,0 +1,13 @@
+/**
+ * Unused for this effect.
+ */
+
+import type { EffectConfigResolver } from 'src/EffectLib/Bloom/Configs';
+
+export async function Start(configResolver: EffectConfigResolver) {
+    
+}
+
+export function Stop() {
+    
+}
